ce44505131ca9e6c5db9bfc2cbf91ef1d8cb45c9
Bart Jeukendrup <bart@jeukendrup.nl>
2015-08-25 15:19:38 +0200

    fixed: correct display of icon, refactored code

--- views/default/icon/object/blog.php
+++ views/default/icon/object/blog.php
@@ -1,118 +1,50 @@
 <?php
+$entity = elgg_extract("entity", $vars);
+$full_view = elgg_extract("full_view", $vars, false);
 
-	$entity = elgg_extract("entity", $vars);
-	$full_view = elgg_extract("full_view", $vars, false);
-	
-	// do we have a blog
-	if (!empty($entity) && elgg_instanceof($entity, "object", "blog")) {
-		$href = elgg_extract("href", $vars, $entity->getURL());
-		$plugin_settings = elgg_extract("plugin_settings", $vars, false);
-		
-		$class = array("blog_tools_blog_image");
-		if (isset($vars["class"])) {
-			$class[] = $vars["class"];
-		}
-		
-		$image_params = array(
-			"alt" => $entity->title,
-			"class" => elgg_extract("img_class", $vars, "")
-		);
-		
-		// does the blog have an image
-		if ($entity->icontime) {
-			// which view
-			if (empty($plugin_settings)) {
-				// default image behaviour
-				$image_params["src"] = $entity->getIconURL(elgg_extract("size", $vars, "medium"));
-			} elseif ($full_view) {
-				// full view of a blog
-				static $blog_tools_full_image_size;
-				static $blog_tools_full_image_align;
-				
-				if (!isset($blog_tools_full_image_size)) {
-					$blog_tools_full_image_size = "large";
-					
-					if ($setting = elgg_get_plugin_setting("full_size", "blog_tools")) {
-						$blog_tools_full_image_size = $setting;
-					}
-				}
-				
-				if (!isset($blog_tools_full_image_align)) {
-					$blog_tools_full_image_align = "right";
-					
-					if ($setting = elgg_get_plugin_setting("full_align", "blog_tools")) {
-						$blog_tools_full_image_align = $setting;
-					}
-				}
-				
-				if ($blog_tools_full_image_align != "none") {
-					$href = false;
+// do we have a blog
+if (!empty($entity) && elgg_instanceof($entity, "object", "blog")) {
+	$href = elgg_extract("href", $vars, $entity->getURL());
 
-					if ($blog_tools_full_image_size == "extralarge") {
-						$image_params["src"] = $entity->getIconURL("master");
-					} else {
-						$image_params["src"] = $entity->getIconURL($blog_tools_full_image_size);
-					}
-					
-					$class[] = "blog-tools-blog-image-" . $blog_tools_full_image_size;
-					
-					if ($blog_tools_full_image_size != "master") {
-						if ($blog_tools_full_image_align == "right") {
-							$class[] = "float-alt";
-						} else {
-							$class[] = "float";
-						}
-					}
-				} else {
-					// no image
-					return true;
-				}
-			} else {
-				// listing view of a blog
-				// full view of a blog
-				static $blog_tools_lising_image_size;
-				static $blog_tools_listing_image_align;
-				
-				if (!isset($blog_tools_lising_image_size)) {
-					$blog_tools_lising_image_size = "small";
-						
-					if ($setting = elgg_get_plugin_setting("listing_size", "blog_tools")) {
-						$blog_tools_lising_image_size = $setting;
-					}
-				}
-				
-				if (!isset($blog_tools_listing_image_align)) {
-					$blog_tools_listing_image_align = "right";
-						
-					if ($setting = elgg_get_plugin_setting("listing_align", "blog_tools")) {
-						$blog_tools_listing_image_align = $setting;
-					}
-				}
-				
-				if ($blog_tools_listing_image_align != "none") {
-					if ($blog_tools_lising_image_size == "extralarge") {
-						$image_params["src"] = $entity->getIconURL("master");
-					} else {
-						$image_params["src"] = $entity->getIconURL($blog_tools_lising_image_size);
-					}
-					
-					$class[] = "blog-tools-blog-image-" . $blog_tools_lising_image_size;
-					
-					if ($blog_tools_listing_image_align == "right") {
-						$class[] = "float-alt";
-					} else {
-						$class[] = "float";
-					}
-				} else {
-					// no image
-					return true;
-				}
-			}
-		}
-		
-		$image = elgg_view("output/img", $image_params);
-		
+	$class = array("blog_tools_blog_image");
+	if (isset($vars["class"])) {
+		$class[] = $vars["class"];
+	}
+
+	$image_params = array(
+		"alt" => $entity->title,
+		"class" => elgg_extract("img_class", $vars, "")
+	);
+
+	$image_params["src"] = $entity->getIconURL(elgg_extract("size", $vars, "medium"));
+
+	if ($full_view) {
+		$size = elgg_get_plugin_setting("full_size", "blog_tools", "large");
+		$align = elgg_get_plugin_setting("full_align", "blog_tools", "right");
+	} else { // listing view
+		$size = elgg_get_plugin_setting("listing_size", "blog_tools", "small");
+		$align = elgg_get_plugin_setting("listing_align", "blog_tools", "right");
+	}
+
+	if ($size == "extralarge") {
+		$image_params["src"] = $entity->getIconURL("master");
+	} else {
+		$image_params["src"] = $entity->getIconURL($size);
+	}
+
+	$class[] = "blog-tools-blog-image-" . $size;
+
+	if ($align == "right") {
+		$class[] = "float-alt";
+	} else {
+		$class[] = "float";
+	}
+
+	// does the blog have an image
+	if ($entity->icontime && $align != "none") {
 		echo "<div class='" . implode(" ", $class) . "'>";
+		$image = elgg_view("output/img", $image_params);
+
 		if (!empty($href)) {
 			$params = array(
 				"href" => $href,
@@ -123,11 +55,14 @@
 			if ($class) {
 				$params["class"] = $class;
 			}
-			
+
 			echo elgg_view("output/url", $params);
 		} else {
 			echo $image;
 		}
-		
+
 		echo "</div>";
+	} else {
+		echo "<!-- do not show blog image -->"; // make sure something is returned
 	}
+}
